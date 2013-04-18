b5502b6688e62808a3940238c97adb7c6f68ed14
Ray <ray@rayps.com>
2013-04-19 01:26:24 +0800

    1.0

---
+++ Code.jsx
@@ -0,0 +1,26 @@
+﻿if(app.documents.length>0){
+    var doc = app.activeDocument
+ //   plus()
+ //   minus()
+    }
+else{}
+
+function plus(){
+    customResizeImage(doc.width*2,doc.height*2)
+    }
+function minus(){
+    customResizeImage(doc.width/2,doc.height/2)
+    }
+
+function customResizeImage(Width,Height) {
+var desc = new ActionDescriptor();
+if(activeDocument.width > activeDocument.height){
+desc.putUnitDouble( charIDToTypeID('Wdth'), charIDToTypeID('#Pxl'), Width );
+}else{
+desc.putUnitDouble( charIDToTypeID('Hght'), charIDToTypeID('#Pxl'), Height);
+}
+desc.putBoolean( stringIDToTypeID('scaleStyles'), true );
+desc.putBoolean( charIDToTypeID('CnsP'), true );
+desc.putEnumerated( charIDToTypeID('Intr'), charIDToTypeID('Intp'), charIDToTypeID('Bcbc') );
+executeAction( charIDToTypeID('ImgS'), desc, DialogModes.NO );
+};
---
+++ README.md
@@ -0,0 +1,53 @@
+#2xScaler
+
+###A photoshop extension for App designers
+
+### Double scale your design with one click.
+
+version 1.0
+
+---
+
+![](README.img/1.png)
+
+##Screen shot
+
+![](README.img/3.png)
+
+
+##Feature
+
+App designers should understand what's it.
+
+![](README.img/2.png)
+
+
+##install
+
+![](README.img/4.png)
+
+[Download the .ZXP file](2xScaler.zxp) and open it
+
+then Adobe extension manager should show up automatically .
+
+##Usage
+
+After installed, you can find it in the photoshop menu:
+####` Window -> Extension -> 2xScaler `
+
+
+click `+`	or	`-` icon to scale.
+
+##Tips
+
+* Please use the default dark UI
+* Please use CS6 because i didn't test it in cs5
+* Any feedback or question is welcome: ray@rayps.com
+
+---
+
+Ray
+
+2013/4/19
+
+[www.rayps.com](http://rayps.com/)
